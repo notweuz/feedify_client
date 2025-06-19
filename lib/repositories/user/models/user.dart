a67b3ccb2f2c89a1271339d057ffd6278013cc71
notweuz <weuz.main@gmail.com>
2025-06-19 22:47:25 +0400

    Update userdto

--- lib/repositories/user/models/user.dart
+++ lib/repositories/user/models/user.dart
@@ -4,17 +4,21 @@ class User {
   final String username;
   final String description;
   final String? avatarUrl;
+  final String? bannerUrl;
   final DateTime registrationDate;
+  final int postsCount;
   final int followersCount;
   final int followingCount;
 
   const User({
     required this.id,
-    required this.displayName,
+    this.displayName,
     required this.username,
     required this.description,
-    required this.avatarUrl,
+    this.avatarUrl,
+    this.bannerUrl,
     required this.registrationDate,
+    required this.postsCount,
     required this.followersCount,
     required this.followingCount,
   });
@@ -26,7 +30,9 @@ class User {
       username: json["username"],
       description: json["description"],
       avatarUrl: json["avatar_url"],
+      bannerUrl: json["banner_url"],
       registrationDate: DateTime.parse(json["registration_date"]),
+      postsCount: json['posts_count'],
       followersCount: json["followers_count"],
       followingCount: json["following_count"],
     );
