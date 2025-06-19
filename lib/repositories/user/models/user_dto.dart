fa8c66dad3b0376d4859d6f4e115f8d80cc4f287
notweuz <weuz.main@gmail.com>
2025-06-19 11:58:55 +0400

    Add profile description functionality from server

--- lib/repositories/user/models/user_dto.dart
+++ lib/repositories/user/models/user_dto.dart
@@ -2,6 +2,7 @@ class UserDTO {
   final int id;
   final String displayName;
   final String username;
+  final String description;
   final String? avatarUrl;
   final DateTime registrationDate;
   final int followersCount;
@@ -11,6 +12,7 @@ class UserDTO {
     required this.id,
     required this.displayName,
     required this.username,
+    required this.description,
     required this.avatarUrl,
     required this.registrationDate,
     required this.followersCount,
@@ -22,6 +24,7 @@ class UserDTO {
       id: json["id"],
       displayName: json["display_name"],
       username: json["username"],
+      description: json["description"],
       avatarUrl: json["avatar_url"],
       registrationDate: DateTime.parse(json["registration_date"]),
       followersCount: json["followers_count"],
